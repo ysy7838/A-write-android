596bf51294c5841ee763263b75f639e56031fc41
sung97338 <sung97338@naver.com>
2024-02-16 20:46:46 +0900

    이름변경

--- app/src/main/java/com/example/a_write/api/NetworkModule.kt
+++ app/src/main/java/com/example/a_write/api/NetworkModule.kt
@@ -3,7 +3,7 @@ package com.example.a_write.api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "http://43.201.161.83:8080/"
+const val BASE_URL = "http://43.201.161.83:8080"
 
 fun getRetrofit(): Retrofit {
 
